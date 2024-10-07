2e575ddf0ffeac1bfc498825d8bc4e0ad1a2a8dd
SYECHYUSUF <mochsyechyusufm@gmail.com>
2024-10-07 21:43:47 +0800

    update fungsi (fungsi panjang)

--- project/fungsi_panjang.py
+++ project/fungsi_panjang.py
@@ -1,4 +1,5 @@
 def panjang(dari_satuan, ke_satuan, nilai):
     
     "km" : {"mm" : 1000000, "cm" : 100000, "dam" : 10000, "m" : 1000, "hm" : 10},
+    "hm" : {"mm" : 10000,   "cm" : 1000,    "dam":  10,   "m" : 100,  "km" : 0.1},
     
