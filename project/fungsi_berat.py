a5bff8d934e554e42558fcbe67634556abdd1f4b
AkmalHstl <akmalrbc6@gmail.com>
2024-10-07 14:25:51 +0800

    membuat dictionary

---
+++ project/fungsi_berat.py
@@ -0,0 +1,10 @@
+konversi = {
+    "mg": {"nama": "Miligram(mg)", "nilai": 1},
+    "cg": {"nama": "Centigram( cg)", "nilai": 10},
+    "dg": {"nama": "Desigram(dg)", "nilai": 100},
+    "g":  {"nama": "Gram(g)", "nilai": 1000},
+    "dag":{"nama": "Dekagram(dag)", "nilai": 10000},
+    "hg": {"nama": "Hektogram(hg)", "nilai": 100000},
+    "kg": {"nama": "Kilogram(kg)", "nilai": 1000000},
+    "ton":{"nama": "Ton", "nilai": 1000000000}
+}
