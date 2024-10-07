9eb7f519398b4222c0a2a11e156dc3e2f466db72
llLanisterll <sarhamkampus@gmail.com>
2024-10-07 14:30:10 +0800

    fungsi waktu

---
+++ project/fungsi_waktu.py
@@ -0,0 +1,6 @@
+konversi_waktu = {
+    "detik": {"nama": "Detik", "nilai": 1},
+    "menit": {"nama": "Menit", "nilai": 60},
+    "jam": {"nama": "Jam", "nilai": 3600},
+    "hari": {"nama": "Hari", "nilai": 86400}
+}
