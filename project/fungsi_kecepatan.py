a11ab51736c3845c5c61c132fe6af063de70d4b8
MONTANA <yudbryawan@BRYAWANMONTANA.local>
2024-10-07 14:29:21 +0800

    fungsi kecepatan

---
+++ project/fungsi_kecepatan.py
@@ -0,0 +1,7 @@
+konversi = {
+    "m/s": {"nama": "Meter per detik (m/s)", "nilai": 1},
+    "km/h": {"nama": "Kilometer per jam (km/h)", "nilai": 3.6},
+    "mph": {"nama": "Mil per jam (mph)", "nilai": 2.23694},
+    "knot": {"nama": "Knot", "nilai": 1.94384},
+    "mach": {"nama": "Mach", "nilai": 0.002915}
+}
