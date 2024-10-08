c00fd114f192445ba844b15d2cc0147f31ac75ca
Assyiekril <arilsarda21@gmail.com>
2024-10-08 13:22:12 +0800

    update fungsi suhu

--- project/fungsi_suhu.py
+++ project/fungsi_suhu.py
@@ -12,3 +12,9 @@ def suhu(satuan1, satuan2, nilai):
         return (nilai - 32) * 5/9 + 273.15
     elif satuan1 == 'f' and satuan2 == 'r':
         return (nilai * 4/9 - 32)
+    elif satuan1 == 'k' and satuan2 == 'c':
+        return (nilai - 273.15)
+    elif satuan1 == 'k' and satuan2 == 'f':
+        return ((nilai - 273.15) * 9/5 + 32)
+    elif satuan1 == 'k' and satuan2 == 'r':
+        return (nilai * 4/5 - 273.15)
