c4429186ccb6675fd3fcdd4d2e5008a3bc007efd
AkmalHstl <akmalrbc6@gmail.com>
2024-10-09 20:24:38 +0800

    menambahkan komentar keterangan kode

--- project/fungsi_berat.py
+++ project/fungsi_berat.py
@@ -1,3 +1,6 @@
+# Definisikan kamus konversi satuan berat
+# Kamus ini berisi pasangan key-value, di mana key adalah singkatan satuan berat
+# dan value adalah dictionary yang berisi nama satuan berat dan nilai konversinya
 konversi = {
     "mg": {"nama": "Miligram(mg)", "nilai": 1},
     "cg": {"nama": "Centigram( cg)", "nilai": 10},
@@ -8,9 +11,13 @@ konversi = {
     "kg": {"nama": "Kilogram(kg)", "nilai": 1000000},
     "ton":{"nama": "Ton", "nilai": 1000000000}
 }
+# Fungsi untuk mengkonversi satuan berat
 def berat(satuan1:str, satuan2:str, nilai:float):
     try:
+        # Membuat variable hasil 
+        # untuk menghitung hasil konversi dengan mengalikan nilai awal dengan nilai satuan1 kemudian dibagi dengan nilai satuan 2
         hasil = nilai * konversi[satuan1]["nilai"] / konversi[satuan2]["nilai"]
-        return f"{hasil:.2f} "
+        return f"{hasil:.2f}"
     except KeyError:
+        # Jika input tidak valid, cetak pesan kesalahan
         print("input tidak valid")
