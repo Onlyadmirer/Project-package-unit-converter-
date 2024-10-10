a51bd1ba4a77435b54b5311406df9c35bf9af90f
AkmalHstl <akmalrbc6@gmail.com>
2024-10-10 18:42:31 +0800

    add main branch

--- project/fungsi_berat.py
+++ project/fungsi_berat.py
@@ -1,21 +1,21 @@
 # Definisikan kamus konversi satuan berat
 # Kamus ini berisi pasangan key-value, di mana key adalah singkatan satuan berat
-# dan value adalah dictionary yang berisi nama satuan berat dan nilai konversinya
+# dan value adalah dictionary yang berisi nilai dari satuan itu
 konversi = {
-    "mg": {"nama": "Miligram(mg)", "nilai": 1},
-    "cg": {"nama": "Centigram( cg)", "nilai": 10},
-    "dg": {"nama": "Desigram(dg)", "nilai": 100},
-    "g":  {"nama": "Gram(g)", "nilai": 1000},
-    "dag":{"nama": "Dekagram(dag)", "nilai": 10000},
-    "hg": {"nama": "Hektogram(hg)", "nilai": 100000},
-    "kg": {"nama": "Kilogram(kg)", "nilai": 1000000},
-    "ton":{"nama": "Ton", "nilai": 1000000000}
+    "mg": {"nilai": 1},
+    "cg": {"nilai": 10},
+    "dg": {"nilai": 100},
+    "g":  {"nilai": 1000},
+    "dag":{"nilai": 10000},
+    "hg": {"nilai": 100000},
+    "kg": {"nilai": 1000000},
+    "ton":{"nilai": 1000000000}
 }
 # Fungsi untuk mengkonversi satuan berat
 def berat(satuan1:str, satuan2:str, nilai:float):
     try:
         # Membuat variable hasil 
-        # untuk menghitung hasil konversi dengan mengalikan nilai awal dengan nilai satuan1 kemudian dibagi dengan nilai satuan 2
+        # untuk menghitung hasil konversi dengan mengalikan nilai awal dari satuan berat dengan nilai satuan1 kemudian dibagi dengan nilai satuan 2
         hasil = nilai * konversi[satuan1]["nilai"] / konversi[satuan2]["nilai"]
         return f"{hasil:.2f}"
     except KeyError:
